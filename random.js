dbb7f066d33062731679b0c50830d5a8fc4d92c1
brewchetta <chett.tiller@gmail.com>
2019-06-29 17:39:57 -0400

    Added random

---
+++ random.js
@@ -0,0 +1,12 @@
+export function random(list) {
+  if (Array.isArray(list)) {
+    return list[Math.floor(Math.random() * list.length)]
+  }
+
+  if (typeof list === "object") {
+    const key = random(Object.keys(list))
+    return {[key]: list[key]}
+  }
+
+  console.error(`TypeError: cannot select a random element from "${list}"`);
+}
