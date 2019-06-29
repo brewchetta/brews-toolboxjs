0403ee24fa6edecce5fa86b5e812559c9ac9710f
brewchetta <chett.tiller@gmail.com>
2019-06-29 17:28:38 -0400

    Added capitalizeWords

---
+++ capitalizeWords.js
@@ -0,0 +1,10 @@
+export function capitalizeWords(string) {
+  if ((typeof string !== 'string') || string.length === 0) return undefined;
+
+  if (string.includes(' ')) {
+    return string.split(' ').map(word => capitalizeWords(word)).join(' ')
+  }
+  else {
+    return [...string[0].toUpperCase(),string.slice(1)].join('')
+  }
+}
