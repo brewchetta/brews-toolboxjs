83b0347ea751e5c9b1b5aac9ca1eb23413a6def1
Chett Tiller <chett.tiller@gmail.com>
2019-07-11 14:43:34 -0400

    Random now works on strings to get a random letter

--- random.js
+++ random.js
@@ -1,11 +1,11 @@
 export function random(list) {
-  if (Array.isArray(list)) {
-    return list[Math.floor(Math.random() * list.length)]
+  if (Array.isArray(list) || typeof list === "string") {
+    return list[Math.floor(Math.random() * list.length)];
   }
 
   if (typeof list === "object") {
-    const key = random(Object.keys(list))
-    return {[key]: list[key]}
+    const key = random(Object.keys(list));
+    return { [key]: list[key] };
   }
 
   console.error(`TypeError: cannot select a random element from "${list}"`);
