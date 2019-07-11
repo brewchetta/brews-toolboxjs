559fe598adf25defc5d682cc74dcfb553161cdc8
Chett Tiller <chett.tiller@gmail.com>
2019-07-11 14:58:30 -0400

    Made string selector more robust so it ignores whitespaces

--- random.js
+++ random.js
@@ -1,11 +1,15 @@
 export function random(list) {
-  if (Array.isArray(list) || typeof list === "string") {
+  if (Array.isArray(list)) {
     return list[Math.floor(Math.random() * list.length)];
   }
 
   if (typeof list === "object") {
     const key = random(Object.keys(list));
     return { [key]: list[key] };
+  }
+
+  if (typeof list === "string") {
+    return list.replace(/ /g, "")[Math.floor(Math.random() * list.length)];
   }
 
   console.error(`TypeError: cannot select a random element from "${list}"`);
