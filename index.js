223685f2a2dfc83affb14bd87e2105ce41299f05
Chett Tiller <chett.tiller@gmail.com>
2019-07-11 15:11:18 -0400

    Removed random test, converted to node package

---
+++ index.js
@@ -0,0 +1,18 @@
+import { capitalizeWords } from "./capitalizeWords";
+import { compareAlphabetical, sortAlphabetical } from "./compareAlphabetical";
+import { isAConsonant } from "./isAConsonant";
+import { isALetter } from "./isALetter";
+import { isAVowel } from "./isAVowel";
+import { nthNumWord } from "./nthNumWord";
+import { numWord } from "./numWord";
+import { random } from "./random";
+
+exports.capitalizeWords = capitalizeWords;
+exports.compareAlphabetical = compareAlphabetical;
+exports.sortAlphabetical = sortAlphabetical;
+exports.isAConsonant = isAConsonant;
+exports.isALetter = isALetter;
+exports.isAVowel = isAVowel;
+exports.nthNumWord = nthNumWord;
+exports.numWord = numWord;
+exports.random = random;
